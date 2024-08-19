acc8a04a2af30802d9c73711454317f48dee2bf0
Firaas2004 <youngishaqbashir@gmail.com>
2024-08-19 13:06:41 +0100

    want to start learning backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,28 @@
-import React, { useState } from "react";
+import React from "react";
 import Nav from "./components/Nav";
 import Logo from "./components/Logo";
 import "./App.css";
 import ParticlesComponent from "./components/particles";
-import ImageLinkForm from "./components/ImageLinkForm";
 import Rank from "./components/Rank";
 
 const App = () => {
-  const [input, setInput] = useState("");
-  const inputHandler = (event) => {
-    const newValue = event.target.value;
-    setInput(newValue);
-    console.log(newValue);
-  };
-  const onSubmit = () => {
-    console.log("hello");
-  };
   return (
     <div>
       <ParticlesComponent id="particles" />
       <Nav />
       <Logo />
       <Rank />
-      <ImageLinkForm
-        input={input}
-        inputHandler={inputHandler}
-        onSubmit={onSubmit}
-      />
+      <div className="flex flex-col justify-center items-center gap-3 ">
+        <p className="text-xl text-center">
+          {"This Magic Brain will detect Face in your picture. Give it a try"}
+        </p>
+        <div className="p-6  rounded-md shadow-md  flex justify-center w-[450px] sm:w-[600px] md:w-[650px] lg:w-[700px] bg-gradient-to-r from-pink-500 to-blue-300">
+          <input type="text" className="w-[70%] py-2" />
+          <button className="bg-purple-500 w-[30%] py-2 hover:bg-purple-700  active:scale-95">
+            Detect
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
